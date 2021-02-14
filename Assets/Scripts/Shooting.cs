3c69f27d4b2dbd58e74ddec97e2134f82a74631a
lucas <lucasset10@msn.com>
2021-02-14 12:01:46 -0300

    Tiro e recarga funcionando

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,18 +7,95 @@ public class Shooting : MonoBehaviour
     // Variaveis de tiro
     public Transform firePoint;
     public GameObject bulletPrefab;
-    public float bulletForce = 20f;
-
-    // Temporizaçao do tiro
-    private float timeToShoot; // tempo de espera para o proximo tiro
-    private const float handgunShootDelay = 0.8f;
-
-    // Variavesi de Muniçao
-    public int handgunBullets = 5;
+    //public float bulletForce = 20f;
 
     private int GunType = 1; // Variavel para seleçao da arma
+    Handgun hg = new Handgun(15, 3, 10, true);
+
     Animator animator;
 
+    public class Weapon
+    {
+        protected int totalAmmo {get; set;}
+        protected int loadedAmmo {get; set;}
+        protected int ammoCapacity { get; set; }
+        protected bool isAvailable { get; set; }
+        public float shootDelay { get; protected set; }
+        public float timeToShoot { get; protected set; } // tempo de espera para o proximo tiro
+
+        public Weapon(int total_ammo, int loaded_ammo, int max_ammo_loaded, bool is_available)
+        {
+            totalAmmo = total_ammo;
+            loadedAmmo = loaded_ammo;
+            ammoCapacity = max_ammo_loaded;
+            isAvailable = is_available;
+        }
+        public virtual void Shoot()
+        {
+            totalAmmo--;
+            loadedAmmo--;
+        }
+
+        public virtual void Shoot(Transform firePoint, GameObject bulletPrefab)
+        {
+        }
+
+        public void Reload()
+        {
+            int ammoToReload = Mathf.Min(ammoCapacity - loadedAmmo, totalAmmo - loadedAmmo);
+            //totalAmmo -= ammoToReload;
+            loadedAmmo += ammoToReload;
+        }
+
+        //public virtual void Reload(Animator animator)
+        //{
+        //}
+
+        public bool CanShoot()
+        {
+            return totalAmmo > 0 && loadedAmmo > 0 && timeToShoot <= 0;
+        }
+
+        public bool CanReload()
+        {
+            return totalAmmo > 0 && Mathf.Min(ammoCapacity - loadedAmmo, totalAmmo - loadedAmmo) > 0 && timeToShoot <= 0;
+        }
+
+        public void CountdownToShoot()
+        {
+            if (timeToShoot >= 0)
+                timeToShoot -= Time.deltaTime;
+        }
+
+        public (int,int) AmmoStats()
+        {
+            return (loadedAmmo, totalAmmo - loadedAmmo);
+        }
+    }
+
+    public class Handgun:Weapon
+    {
+        private float bulletForce = 20f;
+        public Handgun(int total_ammo, int loaded_ammo, int max_ammo_loaded, bool is_available):base(total_ammo, loaded_ammo, max_ammo_loaded, is_available)
+        {
+            shootDelay = 0.6f; // tempo até poder atirar de novo com a pistola
+        }
+
+        //public override void Reload(Animator animator)
+        //{
+        //    base.Reload();
+        //    animator.SetBool("IsReloading", true);
+        //}
+
+        public override void Shoot(Transform firePoint,GameObject bulletPrefab)
+        {
+            base.Shoot();
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
+            timeToShoot = shootDelay;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -28,48 +105,32 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CountdownToShoot();
+        //CountdownToShoot();
+        Weapon currentGun = hg;
+        switch (GunType)
+        {
+            case 1:     currentGun = hg;       break;
+        }
+        currentGun.CountdownToShoot();
         if (Input.GetButtonDown("Fire1"))
         {
-            switch (GunType)
+            if (currentGun.CanShoot())
             {
-                case 1: 
-                    if(handgunBullets > 0 && timeToShoot <= 0)
-                        Shoot();
-                    break;
+                currentGun.Shoot(firePoint,bulletPrefab);
+                animator.SetTrigger("IsShooting");
+                animator.SetInteger("GunType", GunType);
             }
         }
-        else if (Input.GetButtonDown("Reload"))
+        else if (Input.GetKeyDown(KeyCode.R))
         {
-            switch (GunType)
+            if (currentGun.CanReload())
             {
-                case 1:
-                    if (handgunBullets <= 0 && timeToShoot <= 0)
-                        Reload();
-                    break;
+                currentGun.Reload();
+                animator.SetTrigger("IsReloading");
+                animator.SetInteger("GunType", GunType);
             }
         }
-    }
-
-    void Shoot()
-    {
-        animator.SetInteger("GunType", GunType);
-        animator.SetTrigger("IsShooting");
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(firePoint.right * bulletForce, ForceMode2D.Impulse);
-        handgunBullets--;
-        timeToShoot = handgunShootDelay;
-    }
-
-    void Reload()
-    {
-        animator.SetInteger("Reload",GunType);
-    }
-
-    void CountdownToShoot()
-    {
-        if(timeToShoot >=0)
-            timeToShoot -= Time.deltaTime;
+        (int loaded_ammo, int remaining_ammo) = currentGun.AmmoStats();
+        Debug.Log(loaded_ammo + "-" + remaining_ammo);
     }
 }
