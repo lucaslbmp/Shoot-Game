635662e690d42f5fa3807dfbf7e5249daddf29e5
GilmarCorreia <gilmarcorreiajeronimo@gmail.com>
2021-04-19 00:15:22 -0300

    criando dialog box

--- Assets/Scripts/MonoBehaviour/ScriptsAmbiente/GerenciadorDeLuz.cs
+++ Assets/Scripts/MonoBehaviour/ScriptsAmbiente/GerenciadorDeLuz.cs
@@ -3,25 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+/// <summary>
+/// Essa classe gerencia a iluminação inicial do ambiente, deixando uma parte dos tilemaps iluminada e a outra parte dos tilemaps não iluminada
+/// </summary>
+
 public class GerenciadorDeLuz : MonoBehaviour
 {
-    GameObject exteriorCasa;
-    GameObject paredesExternas;
-    GameObject objetosExternos;
+    GameObject exteriorCasa;    // Salva a game object que contém as tilemaps do exterior da casa
+    GameObject paredesExternas; // Salva a game object que contém as tilemaps do muro exterior
+    GameObject objetosExternos; // Salva a game object que contém as tilemaps dos objetos no exterior da casa
 
-    Color iluminado = new Color(0.73f, 0.73f, 0.73f, 1.0f);
-    Color apagado = new Color(0.142f, 0.142f, 0.142f, 1.0f);
+    Color iluminado = new Color(0.73f, 0.73f, 0.73f, 1.0f);  // Cor da tilemap quando o ambiente é iluminável
+    Color apagado = new Color(0.142f, 0.142f, 0.142f, 1.0f); // Cor da tilemap quando o ambiente está apagado
 
     // Start is called before the first frame update
     void Start()
     {
         exteriorCasa = GameObject.Find("Layer_floor");
         paredesExternas = GameObject.Find("Layer_Wall");
-        objetosExternos = GameObject.Find("Layer_objects");
+        objetosExternos = GameObject.Find("Layer_objects"); 
 
         GameObject luzGlobal = GameObject.Find("LuzGlobal");
-        luzGlobal.GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>().intensity = 0.15f;
+        luzGlobal.GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>().intensity = 0.15f; // ajustando a luz global para o início do jogo
 
+
+        // Altera as cores das tilemaps
         exteriorCasa.GetComponent<Tilemap>().color = iluminado;
         paredesExternas.GetComponent<Tilemap>().color = iluminado;
         objetosExternos.GetComponent<Tilemap>().color = iluminado;
