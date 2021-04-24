7d1e2d3da03b79f1822a7cfe754c5591a77bf610
lucas <lucasset10@msn.com>
2021-04-24 18:02:40 -0300

    Adicionando Comentarios 2

--- Assets/Scripts/MonoBehaviour/ScriptsArmas/Bullet.cs
+++ Assets/Scripts/MonoBehaviour/ScriptsArmas/Bullet.cs
@@ -2,7 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//classe que define o comportamento do projétil/bala
+/// <summary>
+/// Autor: Lucas Barboza
+/// Data: 14/02/2021
+/// Classe que gerencia o comportamento do projetil da pistola e AK47
+/// </summary>
+/// 
+
 public class Bullet : MonoBehaviour
 {
     [HideInInspector] public float speed;                             //define a velocidade da bala/projétil
@@ -22,23 +28,24 @@ public class Bullet : MonoBehaviour
     //método que define como que a bala interage com os objetos
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy")) 
+        if (collision.gameObject.CompareTag("Enemy"))                                  // Se colidiu com um inimigo...
         {
-            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
-            if(enemy != null)
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();                  // Obtem o componente Enemy do objeto
+            if (enemy != null)                                                         // Se Enemy nao é null...
             {
-                StartCoroutine(enemy.DanoCaractere(damage,0f));
+                StartCoroutine(enemy.DanoCaractere(damage, 0f));                        // Inicia a corrotina de dano do inimigo
             }
-            Vector3 hitEffectScale = HitStrenght();
+            Vector3 hitEffectScale = HitStrenght();                                     // Calcula a escala do objeto de sangue que será instanciado
             //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.Euler(0, 0, 90));
             GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.Euler(0, 0, 90));
-            effect.transform.localScale = hitEffectScale;
+            effect.transform.localScale = hitEffectScale;                              // Executa o som de "gemido" do zumbi
             HitAudioSource.PlayOneShot(hitSound);                                   // Executa o som de impacto da bala
             Destroy(effect, 0.4f);
             Destroy(gameObject);
         }
     }
 
+    // Calcula a escala do hitEffect com base no dano do projetil
     public Vector3 HitStrenght()
     {
         float scale = damage / baseDamage;
@@ -48,11 +55,11 @@ public class Bullet : MonoBehaviour
     //atualiza periodicamente a velocidade da bala e se algum objeto colidiu com a mesma
     private void FixedUpdate()
     {
-        speed = (transform.position - lastPosition).magnitude / Time.deltaTime;
-        lastPosition = transform.position;
-        if (speed < 5f)
+        speed = (transform.position - lastPosition).magnitude / Time.deltaTime;         // Calcula a velocidade do projetil
+        lastPosition = transform.position;                      // Guarda a ultima posiçao do projetil
+        if (speed < 5f)                                           // Se a velocidade caiu abaixo de 5
         {
-            Destroy(gameObject);
+            Destroy(gameObject);                                    // Destrua o objeto
         }
     }
 }
--- Assets/Scripts/MonoBehaviour/ScriptsArmas/Pellet.cs
+++ Assets/Scripts/MonoBehaviour/ScriptsArmas/Pellet.cs
@@ -2,32 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Autor: Lucas Barboza
+/// Data: 14/02/2021
+/// Classe que gerencia o comportamento do projetil da espingarda
+/// </summary>
+/// 
 public class Pellet : MonoBehaviour
 {
-    float speed;
-    public float damage;
-    float baseDamage = 20f;
-    public GameObject hitEffect;
-    Vector3 lastPosition;
+    float speed;                                        // Velocidade do projetil
+    public float damage;                                // Dano do projetil
+    float baseDamage = 20f;                             // Base de dano do projetil
+    public GameObject hitEffect;                        // Efeito do acerto do projetil (Ex.: sangue)
+    Vector3 lastPosition;                               // Ultima posiçao do projetil
 
-    public AudioClip hitSound;
-    AudioSource HitAudioSource;
+    public AudioClip hitSound;                          // Som do acerto do tiro
+    AudioSource HitAudioSource;                         // AudioSource que executa o som do acerto do tiro
 
     private void Start()
     {
-        HitAudioSource = gameObject.AddComponent<AudioSource>();
+        HitAudioSource = gameObject.AddComponent<AudioSource>();   // Adiciona o componente AudioSource do som de acerto do projetil
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("Enemy"))                                  // Se colidiu com um inimigo...
         {
-            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
-            if (enemy != null)
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();                  // Obtem o componente Enemy do objeto
+            if (enemy != null)                                                         // Se Enemy nao é null...
             {
-                StartCoroutine(enemy.DanoCaractere(damage, 0f));
+                StartCoroutine(enemy.DanoCaractere(damage, 0f));                        // Inicia a corrotina de dano do inimigo
             }
-            Vector3 hitEffectScale = HitStrenght();
+            Vector3 hitEffectScale = HitStrenght();                                     // Calcula a escala do objeto de sangue que será instanciado
             //GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.Euler(0, 0, 90));
             GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.Euler(0, 0, 90));
             effect.transform.localScale = hitEffectScale;                              // Executa o som de "gemido" do zumbi
@@ -37,6 +43,7 @@ public class Pellet : MonoBehaviour
         }
     }
 
+    // Calcula a escala do hitEffect com base no dano do projetil
     public Vector3 HitStrenght()
     {
         float scale = damage / baseDamage;
@@ -46,11 +53,11 @@ public class Pellet : MonoBehaviour
     //atualiza periodicamente a velocidade da bala e se algum objeto colidiu com a mesma
     private void FixedUpdate()
     {
-        speed = (transform.position - lastPosition).magnitude / Time.deltaTime;
-        lastPosition = transform.position;
-        if (speed < 5f)
+        speed = (transform.position - lastPosition).magnitude / Time.deltaTime;         // Calcula a velocidade do projetil
+        lastPosition = transform.position;                      // Guarda a ultima posiçao do projetil
+        if (speed < 5f)                                           // Se a velocidade caiu abaixo de 5
         {
-            Destroy(gameObject);
+            Destroy(gameObject);                                    // Destrua o objeto
         }
     }
 }
